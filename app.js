a124661eb3f4dc27936cb55b0a9b2130dff0a63d
Drozerah <drozerah@gmail.com>
2022-03-16 18:24:00 +0100

    feat: fetch data

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-(() => {
-  console.log('JS is loaded') // !DEBUG
-  // put your code below
-})()
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  const display_response = document.getElementById('fetched-response')
+  const evt = document.getElementById('fetch-event')
+  const ctx = document.getElementById('fetch-context')
+  
+  evt.addEventListener('click', (e) => {
+    e.preventDefault()
+    fetch('http://localhost:8888/api/v1/event')
+    .then((response) => response.json())
+    .then((data) => {
+      display_response.innerHTML = JSON.stringify(data, 2, ' ')
+      hljs.highlightElement(display_response)
+      display_response.parentElement.classList.add('active')
+    }).catch((error) => {
+      console.log(error)
+    })
+  })
+  ctx.addEventListener('click', (e) => {
+    e.preventDefault()
+    fetch('http://localhost:8888/api/v1/context')
+      .then((response) => response.json())
+      .then((data) => {
+        display_response.innerHTML = JSON.stringify(data, 2, ' ')
+        hljs.highlightElement(display_response)
+        display_response.parentElement.classList.add('active')
+      }).catch((error) => {
+        console.log(error)
+      })
+  })
+})
+
