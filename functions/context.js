1d43dea552e174092104aeabe82b3d5b5c84dc29
Drozerah <drozerah@gmail.com>
2022-03-16 19:25:33 +0100

    feat: lambda function handle error

--- functions/context.js
+++ functions/context.js
@@ -1,10 +1,19 @@
 // const { formattedReturn } = require('../utils/netlify_helpers')
 
 exports.handler = async (event, context) => {
-
-  // return context Object
-  return {
-    statusCode: 200,
-    body: JSON.stringify(context)
+  try {
+    // return context Object
+    console.log(context) // !DEBUG
+    return {
+      statusCode: 200,
+      body: JSON.stringify(context)
+    }
+  } catch (error) {
+    // error case
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: {}
+    }
   }
 }
--- functions/event.js
+++ functions/event.js
@@ -1,10 +1,19 @@
 // const { formattedReturn } = require('../utils/netlify_helpers')
 
 exports.handler = async (event, context) => {
-
-  // return event Object
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event)
+  try {
+    // return event Object
+    console.log(event) // !DEBUG
+    return {
+      statusCode: 200,
+      body: JSON.stringify(event)
+    }
+  } catch (error) {
+    // error case
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: {}
+    }
   }
 }
